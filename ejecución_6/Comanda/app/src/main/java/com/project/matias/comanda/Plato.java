722256562c815a39b884509d23902cd5158d39c5
Maalbak <58985484+Maalbak@users.noreply.github.com>
2021-07-23 18:12:55 -0400

    Add files via upload

---
+++ ejecución_6/Comanda/app/src/main/java/com/project/matias/comanda/Plato.java
@@ -0,0 +1,14 @@
+package com.project.matias.comanda;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Plato extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.plato);
+    }
+}
